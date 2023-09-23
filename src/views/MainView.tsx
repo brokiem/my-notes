f47cf80fd338189c9c3ba89c3a049292295ecdf3
brokiem <milyhqvideo@gmail.com>
2023-09-24 07:43:21 +0800

    Split some components for now

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -78,85 +78,17 @@ const MainView = () => {
             New Note
           </button>
           {/* Notes */}
-          <div>
-            <p className={"text-sm mb-2"}>Your notes</p>
-            <div className={"flex flex-col gap-1"}>
-              {
-                notes.map((note) => (
-                  <button
-                    key={note.id}
-                    onClick={() => {
-                      selectNote(notes.find((n) => n.id === note.id)!);
-                      setTitle(note.title);
-                      setContent(note.content);
-                    }}
-                    className={"flex items-center text-sm font-medium bg-[#222327] hover:bg-[#2a2b2e] active:bg-[#4f5052] transition duration-200 w-64 h-[36px] px-3 rounded-full gap-2 truncate align-middle"}>
-                    <span className="material-symbols-outlined">description</span>
-                    {note.title}
-                  </button>
-                ))
-              }
-            </div>
-          </div>
+          <Notes notes={notes} selectNote={selectNote} setTitle={setTitle} setContent={setContent} />
         </div>
 
         <div className={"grow"}>
           {/* Note form */}
           <div className={"flex flex-col bg-[#222327] rounded-md px-1 sticky top-10"}>
-            <input
-              id={"title-input"}
-              className={"text-xl bg-transparent placeholder-[rgba(255,255,255,0.6)] border-0 outline-none focus:ring-0 h-12 rounded-t-md"}
-              type={"text"}
-              placeholder={"Title"}
-              value={title}
-              onChange={(e) => {
-                setTitle(e.target.value);
-
-                if (selectedNote) {
-                  clearTimeout(savedTimeout!);
-                  setSaved(false);
-
-                  const timeoutId = setTimeout(() => {
-                    saveNote({
-                      id: selectedNote.id,
-                      title: e.target.value,
-                      content: selectedNote.content,
-                    });
-                    setSaved(true);
-                  }, 500);
-                  setSavedTimeout(timeoutId);
-                }
-              }} />
-            <textarea
-              id={"content-input"}
-              className={"bg-transparent placeholder-[rgba(255,255,255,0.6)] border-0 outline-none focus:ring-0 rounded-b-md"}
-              rows={10}
-              placeholder={"Type your notes here..."}
-              value={content}
-              onChange={(e) => {
-                setContent(e.target.value);
-
-                if (selectedNote) {
-                  clearTimeout(savedTimeout!);
-                  setSaved(false);
-
-                  const timeoutId = setTimeout(() => {
-                    saveNote({
-                      id: selectedNote.id,
-                      title: selectedNote.title,
-                      content: e.target.value,
-                    });
-                    setSaved(true);
-                  }, 500);
-                  setSavedTimeout(timeoutId);
-                }
-              }}>
-            </textarea>
+            <NoteInput title={title} selectedNote={selectedNote} setTitle={setTitle} setContent={setContent} content={content} savedTimeout={savedTimeout} setSaved={setSaved} saveNote={saveNote} setSavedTimeout={setSavedTimeout} />
 
             {/* Action buttons */}
             <div className={"flex mt-5 mb-1 mx-2 gap-2"}>
-              <Tippy content="Save" delay={50} duration={150} animation={"scale-subtle"} placement={"bottom"}
-                     arrow={false}>
+              <Tippy content="Save" delay={50} duration={150} animation={"scale-subtle"} placement={"bottom"} arrow={false}>
                 <button
                   id={"save-button"}
                   onClick={() => {
@@ -186,6 +118,7 @@ const MainView = () => {
                   </Tippy>
                 )
               }
+
               <div className={"grow"} />
 
               {
@@ -217,6 +150,89 @@ const MainView = () => {
       </div>
     </div>
   );
-};
+}
+
+function Notes({notes, selectNote, setTitle, setContent}: { notes: Note[], selectNote: (note: Note) => void, setTitle: (title: string) => void, setContent: (content: string) => void }) {
+  return (
+    <>
+      <div>
+        <p className={"text-sm mb-2"}>Your notes</p>
+        <div className={"flex flex-col gap-1"}>
+          {
+            notes.map((note) => (
+              <button
+                key={note.id}
+                onClick={() => {
+                  selectNote(notes.find((n) => n.id === note.id)!);
+                  setTitle(note.title);
+                  setContent(note.content);
+                }}
+                className={"flex items-center text-sm font-medium bg-[#222327] hover:bg-[#2a2b2e] active:bg-[#4f5052] transition duration-200 w-64 h-[36px] px-3 rounded-full gap-2 truncate align-middle"}>
+                <span className="material-symbols-outlined">description</span>
+                {note.title}
+              </button>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+function NoteInput({title, selectedNote, setTitle, setContent, content, savedTimeout, setSaved, saveNote, setSavedTimeout}: { title: string, selectedNote: Note | null, setTitle: (title: string) => void, setContent: (content: string) => void, content: string, savedTimeout: number | null, setSaved: (saved: boolean) => void, saveNote: (note: Note | null) => void, setSavedTimeout: (timeout: number | null) => void }) {
+  return (
+    <>
+      <input
+        id={"title-input"}
+        className={"text-xl bg-transparent placeholder-[rgba(255,255,255,0.6)] border-0 outline-none focus:ring-0 h-12 rounded-t-md"}
+        type={"text"}
+        placeholder={"Title"}
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+
+          if (selectedNote) {
+            clearTimeout(savedTimeout!);
+            setSaved(false);
+
+            const timeoutId = setTimeout(() => {
+              saveNote({
+                id: selectedNote.id,
+                title: e.target.value,
+                content: selectedNote.content,
+              });
+              setSaved(true);
+            }, 500);
+            setSavedTimeout(timeoutId);
+          }
+        }}/>
+      <textarea
+        id={"content-input"}
+        className={"bg-transparent placeholder-[rgba(255,255,255,0.6)] border-0 outline-none focus:ring-0 rounded-b-md"}
+        rows={10}
+        placeholder={"Type your notes here..."}
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+
+          if (selectedNote) {
+            clearTimeout(savedTimeout!);
+            setSaved(false);
+
+            const timeoutId = setTimeout(() => {
+              saveNote({
+                id: selectedNote.id,
+                title: selectedNote.title,
+                content: e.target.value,
+              });
+              setSaved(true);
+            }, 500);
+            setSavedTimeout(timeoutId);
+          }
+        }}>
+      </textarea>
+    </>
+  )
+}
 
 export default MainView;
